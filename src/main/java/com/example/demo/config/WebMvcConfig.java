6ae039ef6870539c80698284ee7642c09b0d4fe9
Kimjunseon <junseon0722@gmail.com>
2023-02-21 00:04:33 +0900

    login 기능으로 빠지게 하기

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -1,6 +1,6 @@
 package com.example.demo.config;
 
-import org.springframework.boot.web.servlet.view.MustacheViewResolver;
+import org.springframework.boot.web.servlet.view.MustacheViewResolver; 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -16,5 +17,35 @@ public class IndexController {
 		return "index";
 	}
 	
+	@GetMapping("/user")
+	public @ResponseBody String user() {
+		return "user";
+	}
+	
+	@GetMapping("/admin")
+	public @ResponseBody String admin() {
+		return "admin";
+	}
+	
+	@GetMapping("/manager")
+	public @ResponseBody String manager() {
+		return "manager";
+	}
+	
+	// 스프링시큐리티 해당 주소를 낚아채는데 - securityconfig 파일 생성 후 작동 안함
+	@GetMapping("/login")
+	public @ResponseBody String login() {
+		return "login";
+	}
+	
+	@GetMapping("/join")
+	public @ResponseBody String join() {
+		return "join";
+	}
+	
+	@GetMapping("/joinProc")
+	public @ResponseBody String joinProc() {
+		return "회원가입 완료!";
+	}
 }
 
